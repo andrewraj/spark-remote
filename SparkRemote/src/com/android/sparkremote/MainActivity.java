e7da76887eb03ed3cf1e2ae92c765963cf16cb37
Andrew Raj <me@andrewraj.com>
2014-07-10 11:33:47 +1000

    Added volley library(Testing connection- not working yet)

--- SparkRemote/src/com/android/sparkremote/MainActivity.java
+++ SparkRemote/src/com/android/sparkremote/MainActivity.java
@@ -1,18 +1,34 @@
 package com.android.sparkremote;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.JSONObject;
+
+import com.android.volley.Request.Method;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+
 import android.app.Activity;
-import android.app.ActionBar;
 import android.app.Fragment;
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.DragEvent;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnDragListener;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.Switch;
 
 public class MainActivity extends Activity {
 
+	private static final String TAG = "MainActivity";
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -45,7 +61,7 @@ public class MainActivity extends Activity {
 	/**
 	 * A placeholder fragment containing a simple view.
 	 */
-	public static class PlaceholderFragment extends Fragment {
+	public static class PlaceholderFragment extends Fragment implements OnDragListener, OnClickListener {
 
 		public PlaceholderFragment() {
 		}
@@ -55,7 +71,73 @@ public class MainActivity extends Activity {
 				Bundle savedInstanceState) {
 			View rootView = inflater.inflate(R.layout.fragment_main, container,
 					false);
+			
+			Switch a = (Switch)rootView.findViewById(R.id.sparkSwitch);
+			a.setOnDragListener(this);
+			a.setOnClickListener(this);
+			//a.setOnCheckedChangeListener(listener);
 			return rootView;
 		}
+
+		@Override
+		public boolean onDrag(View v, DragEvent event) {
+			makeCall();
+			return false;
+		}
+
+		@Override
+		public void onClick(View v) {
+			makeCall();
+		}
+		
+		public void makeCall(){
+			// TODO Auto-generated method stub
+			String serverURL = "https://api.spark.io/v1";
+			String deviceID = "/devices/51ff6f065067545734370187";
+			final String accessToken = "ddc13c3a19c8f135af2a4427f5101cfcce1adfe0";
+			
+			
+			
+			// Tag used to cancel the request
+			String tag_json_obj = "json_obj_req";
+			 
+			String url = serverURL + deviceID + "/relay";
+			         
+   
+			         
+			        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Method.POST,
+			                url, new JSONObject(),
+			                new Response.Listener<JSONObject>() {
+			 
+			                    @Override
+			                    public void onResponse(JSONObject response) {
+			                        Log.d(TAG, response.toString());
+			                    }
+			                }, new Response.ErrorListener() {
+			 
+			                    @Override
+			                    public void onErrorResponse(VolleyError error) {
+			                        VolleyLog.d(TAG, "Error: " + error.getMessage());
+			                        // hide the progress dialog
+			                    }
+			                }) {
+			            @Override
+			            protected Map<String, String> getParams() {
+			                Map<String, String> params = new HashMap<String, String>();
+			                params.put("access_token", "ddc13c3a19c8f135af2a4427f5101cfcce1adfe0");
+			                params.put("params", "r1,LOW");
+			 
+			                return params;
+			            }
+			        };
+			 
+			// Adding request to request queue
+			HTTPConnection.getInstance().addToRequestQueue(jsonObjReq, tag_json_obj);
+		}
+		
+		
+		
+		
 	}
+	
 }
